02526e57a6614dcc5760eeff8f2240507bb08682
chenfanghang <20001117@qq.com>
2021-04-18 16:19:13 +0800

    fix:修复无法发送表单数据问题

--- hrun4j-core/src/main/java/io/lematech/httprunner4j/widget/utils/HttpClientUtil.java
+++ hrun4j-core/src/main/java/io/lematech/httprunner4j/widget/utils/HttpClientUtil.java
@@ -1,22 +1,26 @@
 package io.lematech.httprunner4j.widget.utils;
 
-import cn.hutool.core.map.MapUtil;
-import cn.hutool.core.util.StrUtil;
+import cn.hutool.core.bean.BeanUtil;
 import com.alibaba.fastjson.JSON;
-import io.lematech.httprunner4j.widget.i18n.I18NFactory;
-import io.lematech.httprunner4j.common.Constant;
+import com.alibaba.fastjson.JSONObject;
+import io.lematech.httprunner4j.common.DefinedException;
 import io.lematech.httprunner4j.entity.http.HttpConstant;
 import io.lematech.httprunner4j.entity.http.RequestEntity;
+import io.lematech.httprunner4j.entity.http.RequestParameterEntity;
 import io.lematech.httprunner4j.entity.http.ResponseEntity;
-import io.lematech.httprunner4j.common.DefinedException;
+import io.lematech.httprunner4j.widget.i18n.I18NFactory;
 import io.lematech.httprunner4j.widget.log.MyLog;
 import org.apache.http.*;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.CookieStore;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.*;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.cookie.Cookie;
+import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -26,8 +30,9 @@ import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.URI;
 import java.net.URLEncoder;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 /**
@@ -44,34 +49,35 @@ public class HttpClientUtil {
     }
 
     public static ResponseEntity doGet(String url, Map<String, String> headers) {
-        return doGet(url, headers, Collections.EMPTY_MAP);
+        return doGet(url, headers, new RequestParameterEntity());
     }
 
-    public static ResponseEntity doGet(String url, Map<String, String> headers, Map<String, Object> params) {
+    public static ResponseEntity doGet(String url, Map<String, String> headers, RequestParameterEntity requestParams) {
         RequestConfig requestConfig = RequestConfig.custom()
                 .setConnectTimeout(HttpConstant.CONNECT_TIME_OUT)
                 .setSocketTimeout(HttpConstant.SOCKET_TIME_OUT)
                 .build();
-        return doGet(url, headers, params, requestConfig);
+        return doGet(url, headers, requestParams, requestConfig);
     }
 
-    public static ResponseEntity doGet(String url, Map<String, String> headers, Map<String, Object> params, RequestConfig requestConfig) {
-        return doGet(url, headers, params, new BasicCookieStore(), requestConfig);
+    public static ResponseEntity doGet(String url, Map<String, String> headers, RequestParameterEntity requestParams, RequestConfig requestConfig) {
+        return doGet(url, headers, requestParams, new BasicCookieStore(), requestConfig);
     }
 
-    public static ResponseEntity doGet(String url, Map<String, String> headers, Map<String, Object> params, CookieStore httpCookieStore, RequestConfig requestConfig) {
+    public static ResponseEntity doGet(String url, Map<String, String> headers, RequestParameterEntity requestParams, CookieStore httpCookieStore, RequestConfig requestConfig) {
         CloseableHttpClient httpClient = HttpClients
                 .custom()
                 .setDefaultCookieStore(Optional.ofNullable(httpCookieStore).orElse(httpCookieStore = new BasicCookieStore()))
                 .build();
-        String apiUrl = getUrlWithParams(url, params);
-        HttpGet httpGet = new HttpGet(apiUrl);
+        String apiUrl = getUrlWithParams(url, requestParams.getParams());
+        HttpGetWithEntity httpGet = new HttpGetWithEntity(apiUrl);
         httpGet.setConfig(requestConfig);
         if (headers != null && headers.size() > 0) {
             for (Map.Entry<String, String> entry : headers.entrySet()) {
                 httpGet.addHeader(entry.getKey(), entry.getValue());
             }
         }
+        setRequestBody(httpGet, requestParams);
         CloseableHttpResponse response = null;
         try {
             long startTime = System.currentTimeMillis();
@@ -97,58 +103,36 @@ public class HttpClientUtil {
         }
     }
 
+
     public static ResponseEntity doPost(String url) {
-        return doPost(url, Collections.EMPTY_MAP);
+        return doPost(url, new RequestParameterEntity());
     }
 
-    public static ResponseEntity doPost(String url, Map<String, Object> params) {
-        return doPost(url, Collections.EMPTY_MAP, params, new String());
+    public static ResponseEntity doPost(String url, RequestParameterEntity requestParams) {
+        return doPost(url, Collections.EMPTY_MAP, requestParams);
     }
 
-    public static ResponseEntity doPost(String url, Map<String, Object> params, String json) {
-        return doPost(url, Collections.EMPTY_MAP, params, json);
-    }
-
-    public static ResponseEntity doPost(String url, Map<String, String> headers, Map<String, Object> params, String json) {
+    public static ResponseEntity doPost(String url, Map<String, String> headers, RequestParameterEntity requestParams) {
         RequestConfig requestConfig = RequestConfig.custom()
                 .setConnectTimeout(HttpConstant.CONNECT_TIME_OUT)
                 .setSocketTimeout(HttpConstant.SOCKET_TIME_OUT)
                 .build();
-        return doPost(url, headers, params, json, requestConfig);
+        return doPost(url, headers, requestParams, requestConfig);
     }
 
-    public static ResponseEntity doPost(String url, Map<String, String> headers, Map<String, Object> params, String json, RequestConfig requestConfig) {
-        return doPost(url, headers, params, json, new BasicCookieStore(), requestConfig);
+    public static ResponseEntity doPost(String url, Map<String, String> headers, RequestParameterEntity requestParams, RequestConfig requestConfig) {
+        return doPost(url, headers, requestParams, new BasicCookieStore(), requestConfig);
     }
 
-    public static ResponseEntity doPost(String url, Map<String, String> headers, Map<String, Object> params, String json, CookieStore httpCookieStore, RequestConfig requestConfig) {
+    public static ResponseEntity doPost(String url, Map<String, String> headers, RequestParameterEntity requestParams, CookieStore httpCookieStore, RequestConfig requestConfig) {
         CloseableHttpClient httpClient = HttpClients
                 .custom()
                 .setDefaultCookieStore(Optional.ofNullable(httpCookieStore).orElse(httpCookieStore = new BasicCookieStore()))
                 .build();
         HttpPost httpPost;
-        if (!StrUtil.isEmpty(json)) {
-            url = getUrlWithParams(url, params);
-            httpPost = new HttpPost(url);
-            StringEntity jsonContent;
-            try {
-                jsonContent = new StringEntity(json);
-                jsonContent.setContentType("application/json");
-                jsonContent.setContentEncoding(Constant.CHARSET_UTF_8);
-                httpPost.setHeader("Content-Type", "application/json;charset=UTF-8");
-            } catch (UnsupportedEncodingException e) {
-                String exceptionMsg = String.format("unsupported encoding exception :%s", e.getMessage());
-                throw new DefinedException(exceptionMsg);
-            }
-            httpPost.setEntity(jsonContent);
-        } else {
-            httpPost = new HttpPost(url);
-            if (MapUtil.isEmpty(params)) {
-                HttpEntity entityReq = getUrlEncodedFormEntity(params);
-                httpPost.setEntity(entityReq);
-            }
-        }
-
+        url = getUrlWithParams(url, requestParams.getParams());
+        httpPost = new HttpPost(url);
+        setRequestBody(httpPost, requestParams);
         httpPost.setConfig(requestConfig);
         if (headers != null && headers.size() > 0) {
             for (Map.Entry<String, String> entry : headers.entrySet()) {
@@ -181,15 +165,20 @@ public class HttpClientUtil {
     }
 
     public static ResponseEntity doDelete(String url, Map<String, String> headers, RequestConfig requestConfig) {
-        return doDelete(url, headers, new BasicCookieStore(), requestConfig);
+        return doDelete(url, headers, new RequestParameterEntity(), new BasicCookieStore(), requestConfig);
     }
 
-    public static ResponseEntity doDelete(String url, Map<String, String> headers, CookieStore httpCookieStore, RequestConfig requestConfig) {
+    public static ResponseEntity doDelete(String url, Map<String, String> headers, RequestParameterEntity requestParams, RequestConfig requestConfig) {
+        return doDelete(url, headers, requestParams, new BasicCookieStore(), requestConfig);
+    }
+
+    public static ResponseEntity doDelete(String url, Map<String, String> headers, RequestParameterEntity requestParams, CookieStore httpCookieStore, RequestConfig requestConfig) {
         CloseableHttpClient httpClient = HttpClients
                 .custom()
                 .setDefaultCookieStore(Optional.ofNullable(httpCookieStore).orElse(httpCookieStore = new BasicCookieStore()))
                 .build();
-        HttpDelete httpDelete = new HttpDelete(url);
+        url = getUrlWithParams(url, requestParams.getParams());
+        HttpDeleteWithEntity httpDelete = new HttpDeleteWithEntity(url);
         httpDelete.setConfig(requestConfig);
         if (headers != null && headers.size() > 0) {
             for (Map.Entry<String, String> entry : headers.entrySet()) {
@@ -197,6 +186,7 @@ public class HttpClientUtil {
             }
         }
         CloseableHttpResponse response = null;
+        setRequestBody(httpDelete, requestParams);
         try {
             long startTime = System.currentTimeMillis();
             response = httpClient.execute(httpDelete);
@@ -222,38 +212,19 @@ public class HttpClientUtil {
     }
 
 
-    public static ResponseEntity doPut(String url, Map<String, String> headers, Map<String, Object> params, String json, RequestConfig requestConfig) {
-        return doPut(url, headers, params, json, new BasicCookieStore(), requestConfig);
+    public static ResponseEntity doPut(String url, Map<String, String> headers, RequestParameterEntity requestParams, RequestConfig requestConfig) {
+        return doPut(url, headers, requestParams, new BasicCookieStore(), requestConfig);
     }
 
-    public static ResponseEntity doPut(String url, Map<String, String> headers, Map<String, Object> params, String json, CookieStore httpCookieStore, RequestConfig requestConfig) {
+    public static ResponseEntity doPut(String url, Map<String, String> headers, RequestParameterEntity requestParams, CookieStore httpCookieStore, RequestConfig requestConfig) {
         CloseableHttpClient httpClient = HttpClients
                 .custom()
                 .setDefaultCookieStore(Optional.ofNullable(httpCookieStore).orElse(httpCookieStore = new BasicCookieStore()))
                 .build();
         HttpPut httpPut;
-        if (!StrUtil.isEmpty(json)) {
-            url = getUrlWithParams(url, params);
-            httpPut = new HttpPut(url);
-            StringEntity jsonContent;
-            try {
-                jsonContent = new StringEntity(json);
-                jsonContent.setContentType("application/json");
-                jsonContent.setContentEncoding(Constant.CHARSET_UTF_8);
-                httpPut.setHeader("Content-Type", "application/json;charset=UTF-8");
-            } catch (UnsupportedEncodingException e) {
-                String exceptionMsg = String.format("unsupported encoding exception :%s", e.getMessage());
-                throw new DefinedException(exceptionMsg);
-            }
-            httpPut.setEntity(jsonContent);
-        } else {
-            httpPut = new HttpPut(url);
-            if (MapUtil.isEmpty(params)) {
-                HttpEntity entityReq = getUrlEncodedFormEntity(params);
-                httpPut.setEntity(entityReq);
-            }
-        }
-
+        url = getUrlWithParams(url, requestParams.getParams());
+        httpPut = new HttpPut(url);
+        setRequestBody(httpPut, requestParams);
         httpPut.setConfig(requestConfig);
         if (headers != null && headers.size() > 0) {
             for (Map.Entry<String, String> entry : headers.entrySet()) {
@@ -361,7 +332,21 @@ public class HttpClientUtil {
                     .getValue().toString());
             pairList.add(pair);
         }
-        return new UrlEncodedFormEntity(pairList, Charset.forName("UTF-8"));
+        return new UrlEncodedFormEntity(pairList, StandardCharsets.UTF_8);
+    }
+
+    private static void setRequestBody(HttpEntityEnclosingRequestBase request, RequestParameterEntity requestParams) {
+        if (Objects.nonNull(requestParams.getData())) {
+            if (requestParams.getData() instanceof JSONObject) {
+                Map<String, Object> dataMap = JSONObject.parseObject(JSON.toJSONString(requestParams.getData()), Map.class);
+                HttpEntity entityReq = getUrlEncodedFormEntity(dataMap);
+                request.setEntity(entityReq);
+            }
+        }
+        if (Objects.nonNull(requestParams.getJson())) {
+            HttpEntity httpEntity = new StringEntity(JSON.toJSONString(requestParams.getJson()), ContentType.APPLICATION_JSON);
+            request.setEntity(httpEntity);
+        }
     }
 
     /**
@@ -417,8 +402,8 @@ public class HttpClientUtil {
         String method = requestEntity.getMethod();
         String url = requestEntity.getUrl();
         Map<String, String> headers = requestEntity.getHeaders();
-        Map<String, Object> params = requestEntity.getParams();
-        String json = JSON.toJSONString(requestEntity.getJson());
+        RequestParameterEntity requestParameterEntity = new RequestParameterEntity();
+        BeanUtil.copyProperties(requestEntity, requestParameterEntity);
         MyLog.info(String.format(I18NFactory.getLocaleMessage("requestUrl"), requestEntity.getUrl()));
         MyLog.info(String.format(I18NFactory.getLocaleMessage("requestMethod"), requestEntity.getMethod()));
         MyLog.info(String.format(I18NFactory.getLocaleMessage("requestHeader"), requestEntity.getHeaders()));
@@ -426,13 +411,13 @@ public class HttpClientUtil {
         MyLog.info(String.format(I18NFactory.getLocaleMessage("requestParameter"), requestEntity.getParams()));
         MyLog.info(String.format(I18NFactory.getLocaleMessage("requestJson"), requestEntity.getJson()));
         if (HttpConstant.GET.equalsIgnoreCase(requestEntity.getMethod())) {
-            responseEntity = doGet(url, headers, params, initRequestConfig(requestEntity));
+            responseEntity = doGet(url, headers, requestParameterEntity, initRequestConfig(requestEntity));
         } else if (HttpConstant.POST.equalsIgnoreCase(method)) {
-            responseEntity = doPost(url, headers, params, json, initRequestConfig(requestEntity));
+            responseEntity = doPost(url, headers, requestParameterEntity, initRequestConfig(requestEntity));
         } else if (HttpConstant.DELETE.equalsIgnoreCase(method)) {
-            responseEntity = doDelete(url, headers, initRequestConfig(requestEntity));
+            responseEntity = doDelete(url, headers, requestParameterEntity, initRequestConfig(requestEntity));
         } else if (HttpConstant.PUT.equalsIgnoreCase(method)) {
-            responseEntity = doPut(url, headers, params, json, initRequestConfig(requestEntity));
+            responseEntity = doPut(url, headers, requestParameterEntity, initRequestConfig(requestEntity));
         } else if (HttpConstant.HEAD.equalsIgnoreCase(method)) {
         } else if (HttpConstant.OPTIONS.equalsIgnoreCase(method)) {
         }
@@ -447,4 +432,52 @@ public class HttpClientUtil {
 
         return responseEntity;
     }
+}
+
+class HttpGetWithEntity extends HttpEntityEnclosingRequestBase {
+    private final static String METHOD_NAME = "GET";
+
+    public HttpGetWithEntity() {
+        super();
+    }
+
+    public HttpGetWithEntity(final URI uri) {
+        super();
+        setURI(uri);
+    }
+
+    HttpGetWithEntity(final String uri) {
+        super();
+        setURI(URI.create(uri));
+    }
+
+    @Override
+    public String getMethod() {
+        return METHOD_NAME;
+    }
+
+}
+
+class HttpDeleteWithEntity extends HttpEntityEnclosingRequestBase {
+    private final static String METHOD_NAME = "DELETE";
+
+    public HttpDeleteWithEntity() {
+        super();
+    }
+
+    public HttpDeleteWithEntity(final URI uri) {
+        super();
+        setURI(uri);
+    }
+
+    HttpDeleteWithEntity(final String uri) {
+        super();
+        setURI(URI.create(uri));
+    }
+
+    @Override
+    public String getMethod() {
+        return METHOD_NAME;
+    }
+
 }
