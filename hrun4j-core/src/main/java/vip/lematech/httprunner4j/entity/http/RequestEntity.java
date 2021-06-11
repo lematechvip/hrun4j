d21c21234f1ed551c771bc84012a5ad988e3a48c
chenfanghang <20001117@qq.com>
2021-06-11 17:08:28 +0800

    feat: convert swagger to API

--- hrun4j-core/src/main/java/vip/lematech/httprunner4j/entity/http/RequestEntity.java
+++ hrun4j-core/src/main/java/vip/lematech/httprunner4j/entity/http/RequestEntity.java
@@ -1,8 +1,9 @@
 package vip.lematech.httprunner4j.entity.http;
 
 
-import vip.lematech.httprunner4j.helper.LittleHelper;
+import com.alibaba.fastjson.annotation.JSONField;
 import lombok.Data;
+import vip.lematech.httprunner4j.helper.LittleHelper;
 
 import java.util.Map;
 
@@ -13,28 +14,35 @@ import java.util.Map;
  */
 @Data
 public class RequestEntity<T> {
+    @JSONField(ordinal = 2)
     private String method;
+    @JSONField(ordinal = 1)
     private String url;
     /**
      * TODO: 2021/1/20 可选键值对（对象）
      * 1、query string for request url
      */
-    private Map<String,Object> params;
+    @JSONField(ordinal = 4)
+    private Map<String, Object> params;
     /**
      * 1、request body in json format
      * 2、request body in application/x-www-form-urlencoded format
      * 3、request body prepared with function, or reference a variable
      */
+    @JSONField(ordinal = 6)
     private T data;
 
     /**
      * 1、request body in json format
      * 2、request body prepared with function, or reference a variable
      */
+    @JSONField(ordinal = 5)
     private T json;
 
+    @JSONField(ordinal = 3)
     private Map<String, Object> headers;
 
+    @JSONField(ordinal = 8)
     private Integer connectionRequestTimeout;
 
     public Integer getConnectionRequestTimeout() {
@@ -49,36 +57,44 @@ public class RequestEntity<T> {
         return LittleHelper.s2ms(this.socketTimeout);
     }
 
+    @JSONField(ordinal = 9)
     private Integer connectTimeout;
 
+    @JSONField(ordinal = 10)
     private Integer socketTimeout;
 
     /**
      * Enable/disable GET/OPTIONS/POST/PUT/PATCH/DELETE/HEAD redirection. Defaults to True
      */
+    @JSONField(ordinal = 11)
     private Boolean allowRedirects = true;
 
     /**
      * request cookies
      */
+    @JSONField(ordinal = 12)
     private Map<String, Object> cookies;
 
     /**
      * request files, used to upload files
      */
+    @JSONField(ordinal = 7)
     private Object files;
     /**
      *
      */
+    @JSONField(ordinal = 13)
     private Object auth;
     /**
      * Dictionary mapping protocol to the URL of the proxy
      */
+    @JSONField(ordinal = 14)
     private Map<String, Object> proxy;
 
     /**
      * if False, the response content will be immediately downloaded.
      */
+    @JSONField(ordinal = 15)
     private Boolean stream = true;
 
 }
--- hrun4j-core/src/main/java/vip/lematech/httprunner4j/entity/testcase/ApiModel.java
+++ hrun4j-core/src/main/java/vip/lematech/httprunner4j/entity/testcase/ApiModel.java
@@ -1,25 +1,29 @@
 package vip.lematech.httprunner4j.entity.testcase;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 import vip.lematech.httprunner4j.entity.base.BaseModel;
 import vip.lematech.httprunner4j.entity.http.RequestEntity;
-import lombok.Data;
 
 import java.util.List;
 import java.util.Map;
 
 /**
- *
  * website http://lematech.vip/
+ *
  * @author lematech@foxmail.com
  * @version 1.0.0
  */
 @Data
 public class ApiModel<T> extends BaseModel {
     @JsonProperty(value = "name")
+    @JSONField(ordinal = 1)
     private String name;
     @JsonProperty(value = "base_url")
+    @JSONField(ordinal = 2)
     private String baseUrl;
+
     /**
      * array[map<k,array>]：used to validate response fields,validate_func_name: [check_value, expect_value]
      * - equalTo: [statusCode, "200"]
@@ -28,7 +32,10 @@ public class ApiModel<T> extends BaseModel {
      * "comparator": "equalTo"
      * "expect": "200"
      */
+    @JSONField(ordinal = 5)
     private List<Map<String, Object>> validate;
+    @JSONField(ordinal = 3)
     private RequestEntity request;
+    @JSONField(ordinal = 4)
     private T extract;
 }
